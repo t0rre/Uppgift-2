09e95b1a1e77cbe64213b15b35ca924b90c3f877
Tor Smedberg <tor.smedberg@gmail.com>
2020-09-17 10:47:58 +0000

    Added service worker registration

--- script.js
+++ script.js
@@ -19,3 +19,14 @@ function light()
     document.getElementById("light").style.visibility = "hidden";
     document.getElementById("w3img").src = "https://jigsaw.w3.org/css-validator/images/vcss";
 }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
