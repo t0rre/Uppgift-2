2615f953daa94d9641e400cd320366febc36a7d9
Tor Smedberg <tor.smedberg@gmail.com>
2020-09-17 11:41:52 +0000

    added caching with serviceworker

--- sw.js
+++ sw.js
@@ -1,4 +1,25 @@
-self.addEventListener('install', function(event)
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(
+        [
+          '/Uppgift-2/index.html',
+          '/Uppgift-2/css_large.png',
+          '/Uppgift-2/css_small.png',
+          '/Uppgift-2/css.png',
+          '/Uppgift-2/e_uppgit.png',
+          '/Uppgift-2/index.html',
+          '/Uppgift-2/manifest.webmanifest',
+          '/Uppgift-2/script.js',
+          '/Uppgift-2/stylesheet.css',
+          '/Uppgift-2/sw.js'
+        ]
+      );
+    })
+  );
+});
+self.addEventListener('start_url', function(event)
 {
-  console.log("ok")
+  console.log("start_url requested")
+
 });
